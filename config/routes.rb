db383143f5d36cf90a6026ccd53cff45cf06feeb
Muhammad Zunair <zunair.dev@gmail.com>
2023-09-07 14:00:47 +0500

    authentication: added home page & followed devise documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root to: "home#index"
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
